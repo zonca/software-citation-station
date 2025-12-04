78c926652e7747ba4b46ef147f344fbb2816048b
Andrea Zonca <code@andreazonca.com>
2025-12-03 16:51:25 -0800

    Remove unused ZenodoVersion import

--- src/fetchVersions.ts
+++ src/fetchVersions.ts
@@ -5,7 +5,7 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
-import { getZenodoVersionInfo, ZenodoVersion } from './zenodoVersions';
+import { getZenodoVersionInfo } from './zenodoVersions';
 
 interface PackageInfo {
   zenodo_doi: string;
